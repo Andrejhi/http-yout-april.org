f7c1eb088676deb1f08bfcf94859474bfcde6d78
Andrejchik <room861@yandex.ru>
2021-04-24 17:25:52 +0300

    add key youtube

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
     //     wrapper.textContent += listVideo[i];
     // }
 const API_KEY = 'AIzaSyC6VoFheCeVMi_QXCGsVW99pKmZHXTY-nU';
+const CLIENT_ID = '96919317985-6kvtmnmem0suvu8tj0icu4ne73ctqchi.apps.googleusercontent.com';
 
 const gloAcademyList = document.querySelector('.glo-academy-list');
 
@@ -37,6 +38,9 @@ const createCard = (dataVideo) => {
 
 // https://console.cloud.google.com/getting-started получение API ключа
 // AIzaSyC6VoFheCeVMi_QXCGsVW99pKmZHXTY-nU API key Apr 23, 2021
+// https://console.cloud.google.com/apis/credentials/oauthclient?previousPage=%2Fapis%2Fcredentials%3Fproject%3Dyoutube-april-2021&project=youtube-april-2021
+// 96919317985-6kvtmnmem0suvu8tj0icu4ne73ctqchi.apps.googleusercontent.com  cliend id
+// nO0BFU3Q1T-7suDOdRh9qnoD client secret
 
 const createList = (wrapper, listVideo) => {
     wrapper.textContent = '';
